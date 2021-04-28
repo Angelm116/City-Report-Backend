64059ae17617a5fe9a59028ffadcc4410a0a3c5b
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-04-28 00:55:41 -0400

    a commit

--- server/db.config.js
+++ server/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
   HOST: "city-report.c8yml2raj5dm.us-east-1.rds.amazonaws.com",
   USER: "admin",
   PASSWORD: "G41WYmkv6uo8JWrTfd2X",
-  DB: "city_report"
+  DB: "city-report"
 };
