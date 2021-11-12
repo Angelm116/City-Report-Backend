c2f7b81015fe56b2d47393b2808da8c187e31d11
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-11-11 19:45:29 -0500

    updated cors

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,7 @@ var reportsController = require('../controllers/reportController.js');
 var router = express.Router();
 
 
+
 router.get('/api/map', reportsController.list_all_reports);
 
 router.post('/api/upload-report', reportsController.create_a_report);
