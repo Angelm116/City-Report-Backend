7b0c8a6bbbacbab24283f59efa9176af0297585b
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-11-12 15:55:22 -0500

    fixed db config

--- server/db.config.js
+++ server/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
   HOST: "city-report-db.cbfpv7jn39fv.us-east-2.rds.amazonaws.com",
   USER: "cradmin",
   PASSWORD: "MasterPassword2021",
-  DB: "city-report-schema"
+  DB: "report_schema"
 };
