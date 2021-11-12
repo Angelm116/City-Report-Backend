b958f8b55598dc1d0e1d9eecdd2a42b6bacd9c5f
Angelm116 <44384080+Angelm116@users.noreply.github.com>
2021-11-11 19:18:19 -0500

    fixed /api/

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,9 +3,9 @@ var reportsController = require('../controllers/reportController.js');
 var router = express.Router();
 
 
-router.get('/map', reportsController.list_all_reports);
+router.get('/api/map', reportsController.list_all_reports);
 
-router.post('/upload-report', reportsController.create_a_report);
+router.post('/api/upload-report', reportsController.create_a_report);
 
 
 
